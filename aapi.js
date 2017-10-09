6ec4c8084f1b4abd9a0caa2f2379feee88b6bdf2
Robert Crooks <rcrooks@brightcove.com>
2017-10-09 07:15:50 -0400

    wip

--- aapi.js
+++ aapi.js
@@ -1,9 +1,24 @@
 var fileinclude = require('gulp-file-include'),
     DOMParser = require('xmldom').DOMParser,
-    gulp = require('gulp');
+    gulp = require('gulp'),
+    styleClasses = {
+        table: 'bcls-table',
+        thead: 'bcls-table__head',
+        tbody: 'bcls-table__body',
+        section: 'bcls-section',
+        codeHttp: 'language-http',
+        codeJson: 'language-json'
+    },
+    colors = {
+        get: '#129129',
+        post: '#4A5BBD',
+        put: '#E4B842',
+        patch: '#364885',
+        delete: '#9B2556'
+    };
 
     gulp.task('fileinclude', function() {
-      gulp.src(['index.html'])
+      gulp.src(['index.html']);
         .pipe(fileinclude({
           prefix: '@@',
           basepath: '@file'
